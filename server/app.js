ebc48781d609dc6d869c36bd94da4143c281bbf3
ssamio <sami.o.seppala@proton.me>
2023-07-31 15:42:03 +0300

    Deployment fixes

--- server/app.js
+++ server/app.js
@@ -20,29 +20,31 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.resolve("..", "client", "build")));
-    app.get("*", (req, res) => 
-    res.sendFile(path.resolve("..", "client", "build", "index.html")));
-}
-else if(process.env.NODE_ENV === "development"){
-    var corsOptions ={
-        origin: "http://localhost:3000",
-        optionsSuccessStatus: 200,
-    };
-    app.use(cors(corsOptions));
-}
-
-var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
+
+app.use(express.static(path.resolve("..", "client", "build")));
+
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
+
+
+if(process.env.NODE_ENV === "production"){
+    app.get("*", (req, res) => 
+    res.sendFile(path.resolve("..", "client", "build", "index.html")));
+}
+
+//Remove below before entering real world
+var corsOptions ={
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 
 module.exports = app;
